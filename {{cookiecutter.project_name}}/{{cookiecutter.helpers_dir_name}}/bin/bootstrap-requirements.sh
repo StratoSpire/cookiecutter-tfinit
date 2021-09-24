f6dc3b711b6bb36269f65be6bbcc8905172d9b02
Lilith Moon Cohen <lilithmooncohen@gmail.com>
2021-09-24 15:49:40 -0600

    adding group and user configuration to iam bootstrap

--- {{cookiecutter.project_name}}/{{cookiecutter.helpers_dir_name}}/bin/bootstrap-requirements.sh
+++ {{cookiecutter.project_name}}/{{cookiecutter.helpers_dir_name}}/bin/bootstrap-requirements.sh
@@ -7,15 +7,20 @@ source "${THIS_SCRIPT_DIR}/set-env-vars.sh"
 
 # AdministratorAccess Role
 aws cloudformation deploy \
---region "${AWS_DEFAULT_REGION}" \
---stack-name "${TFINIT_NAMESPACE}-tfinit-iam" \
---template-file "${TFINIT_HELPERS_FILES_DIR}/tfinit-iam.yaml" \
---parameter-overrides "RoleName=${TFINIT_ROLE_NAME}" \
---capabilities "CAPABILITY_NAMED_IAM"
+    --region "${AWS_DEFAULT_REGION}" \
+    --stack-name "${TFINIT_NAMESPACE}-tfinit-iam" \
+    --template-file "${TFINIT_HELPERS_FILES_DIR}/tfinit-iam.yaml" \
+    --parameter-overrides \
+        UserName=${TFINIT_USER_NAME} \
+        GroupName=${TFINIT_GROUP_NAME} \
+        RoleName=${TFINIT_ROLE_NAME} \
+    --capabilities "CAPABILITY_NAMED_IAM"
 
 # Terraform Remote State Backend
 aws cloudformation deploy \
---region "${AWS_DEFAULT_REGION}" \
---stack-name "${TFINIT_NAMESPACE}-tfinit-terraform-backend" \
---template-file "${TFINIT_HELPERS_FILES_DIR}/tfinit-terraform-backend.yaml" \
---parameter-overrides StateBucketName=${TFINIT_STATE_BUCKET} StateTableName=${TFINIT_STATE_TABLE}
+    --region "${AWS_DEFAULT_REGION}" \
+    --stack-name "${TFINIT_NAMESPACE}-tfinit-terraform-backend" \
+    --template-file "${TFINIT_HELPERS_FILES_DIR}/tfinit-terraform-backend.yaml" \
+    --parameter-overrides \
+        StateBucketName=${TFINIT_STATE_BUCKET} \
+        StateTableName=${TFINIT_STATE_TABLE}
